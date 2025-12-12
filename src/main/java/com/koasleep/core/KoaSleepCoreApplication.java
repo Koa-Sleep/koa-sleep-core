e4df4a0e7064d21f927c5283c986cfd8e0dc6074
loganhagen <loganhagen@gmail.com>
2025-12-12 17:44:03 -0500

    first commit

---
+++ src/main/java/com/koasleep/core/KoaSleepCoreApplication.java
@@ -0,0 +1,13 @@
+package com.koasleep.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoaSleepCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoaSleepCoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/koasleep/core/KoaSleepCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koasleep.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoaSleepCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
